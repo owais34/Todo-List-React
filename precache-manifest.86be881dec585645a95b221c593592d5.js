5f899045fef3f512879ef645fa7c2cae12fdbc2e
owais34 <owaishunter78@gmail.com>
2020-08-17 10:55:08 +0530

    Updates

---
+++ precache-manifest.86be881dec585645a95b221c593592d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749e9f7f80baa8ddff2085e33faafe3d",
+    "url": "/Todo-List-React/index.html"
+  },
+  {
+    "revision": "e4b07d4d07498baa503e",
+    "url": "/Todo-List-React/static/css/main.b2e5d3c1.chunk.css"
+  },
+  {
+    "revision": "474b9c5fd6f21296c9e6",
+    "url": "/Todo-List-React/static/js/2.a566ec49.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo-List-React/static/js/2.a566ec49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4b07d4d07498baa503e",
+    "url": "/Todo-List-React/static/js/main.5b626225.chunk.js"
+  },
+  {
+    "revision": "fb8809d2ae0843382b10",
+    "url": "/Todo-List-React/static/js/runtime-main.0eb7ca33.js"
+  }
+]);
